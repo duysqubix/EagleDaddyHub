9ced91508679729bee66a020bf072bee160014ab
duysqubix <duys@qubixat.com>
2021-01-05 22:51:56 -0600

    added django-extensions and updated requirements.txt

--- eagledaddy/settings.py
+++ eagledaddy/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
     'django.contrib.messages', 'django.contrib.staticfiles',
-    "pydaddy.apps.PydaddyConfig"
+    'django_extensions', "pydaddy.apps.PydaddyConfig"
 ]
 
 MIDDLEWARE = [
