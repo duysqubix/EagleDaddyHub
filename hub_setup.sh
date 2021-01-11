4d04939cdc78a82706d29160096371c25cb0cc99
duysqubix <duys@qubixat.com>
2021-01-11 14:01:12 +0000

    working workflow that takes an uninstalled sd card; flashes it and turns it into a production ready EagleDaddy Core Hub device.

--- hub_setup.sh
+++ hub_setup.sh
@@ -5,8 +5,10 @@
 # Installs necessary packages and setups production ready EagleDaddy
 #
 #
-
-HOME=/home/eagledaddy
+USER=pi
+HOME=/home/$USER
+EG_TMP=/tmp/eagledaddy
+EG_DIR=$HOME/eagledaddy
 
 write(){
 
@@ -25,13 +27,12 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-cd $HOME
 
 # update system
-apt update && apt upgrade -y
+apt update 
 
-# install git
-apt install -y git libffi-dev libssl-dev python3-dev python3 python3-pip
+# install necessary packages
+apt install -y libffi-dev libssl-dev python3-dev python3 python3-pip
 
 # install docker
 curl https://get.docker.com | sh 
@@ -39,18 +40,27 @@ curl https://get.docker.com | sh
 # install docker-compose
 pip3 install docker-compose
 
-# # add new user
-useradd --groups dialout,docker,sudo -d /home/eagledaddy -s /bin/bash -p $(echo eagledaddy | openssl passwd -1 -stdin) eagledaddy
-# # lock pi user, so we can't access account anymore
-passwd -l pi
+# # # add new user
+# useradd --groups dialout,docker,sudo -d /home/$USER -s /bin/bash -p $(echo eagledaddy | openssl passwd -1 -stdin) eagledaddy
+# # # lock pi user, so we can't access account anymore
+# passwd -l pi
+# write "Remember to change password with 'passwd' for security reasons."
 
-write "Remember to change password with 'passwd' for security reasons."
+# add user to docker group
+usermod -aG docker $USER
 
-git clone https://github.com/duysqubix/EagleDaddy.git; cd EagleDaddy
+mkdir -p $EG_DIR
+mkdir -p $EG_TMP
+tar -xzvf eagledaddy.tar.gz -C / # moves to /tmp folder
+mv $EG_TMP/* $EG_DIR
+mv .env $EG_DIR
+chown -R $USER $EG_DIR
+echo "EG_DIR: $EG_DIR, EG_TMP: $EG_TMP, USER: $USER"
 
-# build and run docker
-docker-compose up -d --build
 
+# reload 
+# build and run docker as eagledaddy user
+docker-compose -f $EG_DIR/docker-compose.yml --project-name eagledaddy_hub up --build --detach
 exit 0
 
 
