406423737bd965abfe0f3a766e4b6d30fe49e184
duysqubix <duys@qubixat.com>
2021-01-15 17:42:15 +0000

    added sudo to install file

--- hub_setup.sh
+++ hub_setup.sh
@@ -22,50 +22,19 @@ write(){
 
 
 init_ap(){
+    apt install -y hostapd dnsmasq 
+    # DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
 
+    systemctl unmask hostapd.service
+    systemctl enable hostapd.service
     #### configure hostapd
 
-    # tell dhcp to ignore wlan0 interface, we will set ip address elsewhere
-    echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf 
-
-    # set static IP address for WiFi interface
-    cat << EOF >> /etc/network/interfaces
-    auto lo
-    iface lo net loopback
-
-    auto eth0
-    iface eth0 inet dhcp
-
-    allow-hotplug wlan0
-    iface wlan0 inet static
-        address 192.168.5.1
-        netmask 255.255.255.0
-        network 192.168.5.0
-        broadcast 192.168.5.255
+    # tell dhcp to set static ip address
+        cat << EOF >> /etc/dhcpcd.conf
+interface wlan0
+    static ip_address=192.168.4.1/24
+    nohook wpa_supplicant
 EOF
-
-
-    # configure hostapd.conf
-    cat << EOF >> /etc/hostapd/hostapd.conf
-    interface=wlan0
-    driver=n180211
-    ssid=EagleDaddyHub
-    hw_mode=g
-    channel=10
-    ieee80211n=1
-    wmm_enabled=1
-    ht_capab=[HT40][SHORT-GI-20][DSSS_CCK-40]
-    macaddr_acl=0
-    ignore_broadcast_ssid=0
-    wpa=2
-    wpa_key_mgmt=WPA-PSK
-    wpa_passphrase=eagledaddy
-    rsn_pairwise=CCMP
-EOF
-
-    # updated DAEMON_CONF in default hostapd so it knows where to find
-    # configuration files
-    sed -i "/DAEMON_CONF=/c\DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" /etc/default/hostapd
 
     #### configure dnsmasq
     write "configuring dns server"
@@ -73,20 +42,36 @@ EOF
 
     # 
     cat << EOF >> /etc/dnsmasq.conf
-    interface=wlan0
-    listen-address=192.168.5.1
-    bind-interfaces
-    server=8.8.8.8
-    domain-needed
-    bogus-priv
-    local=/eagledaddy/
-    domain=eagledaddy
-    dhcp-range=192.168.5.100,192.168.5.200,24h
+interface=wlan0
+dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
+domain=eagledaddy
+address=/hub.eagledaddy/192.168.4.1
+EOF
+    # enable 5Ghz
+    write "enabling 5Ghz"
+    rfkill unblock wlan
+
+    # configure hostapd.conf
+    cat << EOF >> /etc/hostapd/hostapd.conf
+country_code=US
+interface=wlan0
+ssid=EagleDaddyHub
+hw_mode=g
+channel=7
+auth_algs=1
+macaddr_acl=0
+ignore_broadcast_ssid=0
+wpa=2
+wpa_key_mgmt=WPA-PSK
+wpa_passphrase=eagledaddy
+wpa_pairwise=TKIP
+rsn_pairwise=CCMP
 EOF
 
-    # add static domain name for this device
-    # so we can address AP as hub.eagledaddy etc...
-    echo "192.168.5.1 hub.eagledaddy hub" >> /etc/hosts
+    # updated DAEMON_CONF in default hostapd so it knows where to find
+    # configuration files
+    sed -i "/DAEMON_CONF=/c\DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"" /etc/default/hostapd
+
 }
 
 
@@ -101,7 +86,7 @@ fi
 apt update 
 
 # install necessary packages
-apt install -y libffi-dev libssl-dev python3-dev python3 python3-pip hostapd dnsmasq ufw
+apt install -y libffi-dev libssl-dev python3-dev python3 python3-pip
 
 # install docker
 if ! command -v docker &> /dev/null
@@ -127,12 +112,12 @@ usermod -aG docker $USER
 
 #### set up device as an Access Point using hostapd/dnsmasq
 write 'Configuring device as an Access Point'
-# init_ap # DO NOT RUN YET, UNTIL YOU CAN ACCESS WEB SITE FROM LOCALNETWORK FIRST
+init_ap
+
 
 ### allow port 80 exposed using ufw
-ufw allow 80
-ufw allow 8000
-ufw enable
+iptables -A INPUT -p tcp --dport 80 --jump ACCEPT # to manually open web port
+iptables-save
 write "initializing ports... `sleep 5`" # wait for ufw to take affect
 
 # copy over eagledaddysrc code to WORKDIR
@@ -140,11 +125,11 @@ mkdir -p $EG_DIR
 mkdir -p $EG_TMP
 tar -xzvf eagledaddy.tar.gz -C / # moves to /tmp folder
 mv $EG_TMP/* $EG_DIR
-cp .env $EG_DIR
 chown -R $USER $EG_DIR
 
 # build and run docker as eagledaddy user
-docker-compose -f $EG_DIR/docker-compose.yml --project-name eagledaddy_hub up --detach
+docker-compose -f $EG_DIR/docker-compose.yml --project-name eagledaddy_hub build
+reboot now
 exit 0
 
 
